fb2c79cc268396780a060781775365d3ef9c2490
shivendra1245 <shivendrasinghkhichi@gmail.com>
2022-04-06 12:24:17 +0530

    get on the track

---
+++ index.js
@@ -0,0 +1,19 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+console.log(saveEl)
+
+function increment() {
+    count += 1
+    countEl.textContent= count
+}
+
+function save(){
+    let countStr = count + "- "
+    saveEl.innerText += countStr
+    countEl.textContent = 0
+    count = 0
+  
+}       
+console.log("lets count people on the subway")
